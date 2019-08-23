dd565ba54f361dd822767d875bf88d3f649cd04a
mir00r <abdur.razzaq06@gmail.com>
2019-08-23 12:02:31 +0600

    create the initial cuoertino application and related styles and dependencies

---
+++ cupertino_store/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+import 'app.dart';
+
+void main() {
+  // This app is designed only to work vertically, so we limit
+  // orientations to portrait up and down.
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  return runApp(CupertinoStoreApp());
+}
+
+class CupertinoStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
