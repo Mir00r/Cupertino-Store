5f70bf41ccc057b98d5eabfd267269774dbeaf2f
mir00r <abdur.razzaq06@gmail.com>
2019-08-23 14:09:14 +0600

    create 3 tab structure

--- cupertino_store/lib/app.dart
+++ cupertino_store/lib/app.dart
@@ -1,3 +1,6 @@
+import 'package:cupertino_store/product_list_tab.dart';
+import 'package:cupertino_store/search_tab.dart';
+import 'package:cupertino_store/shopping_cart_tab.dart';
 import 'package:flutter/cupertino.dart';
 import 'styles.dart';
 
@@ -11,13 +14,57 @@ class CupertinoStoreApp extends StatelessWidget {
 }
 
 class CupertinoStoreHomePage extends StatelessWidget {
-  @override
+  /*@override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: const Text('Cupertino Store'),
       ),
       child: Container(),
+    );
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            title: Text('Products'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            title: Text('Search'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            title: Text('Cart'),
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: ProductListTab(),
+              );
+            });
+          case 1:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+          case 2:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: ShoppingCartTab(),
+              );
+            });
+        }
+      },
     );
   }
 }
