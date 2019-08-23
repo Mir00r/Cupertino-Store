77235f5070afa48fb422159d92580d1ea41e209c
mir00r <abdur.razzaq06@gmail.com>
2019-08-24 00:51:28 +0600

    prepare product list screen using CupertinoSliverNavigationBar and SliverSafeArea library

--- cupertino_store/lib/product_list_tab.dart
+++ cupertino_store/lib/product_list_tab.dart
@@ -1,3 +1,4 @@
+import 'package:cupertino_store/product_row_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
@@ -6,16 +7,39 @@ import 'model/app_state_model.dart';
 class ProductListTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppStateModel>(
-      builder: (context, model, child) {
-        return CustomScrollView(
-          slivers: const <Widget>[
-            CupertinoSliverNavigationBar(
-              largeTitle: Text('Cupertino Store'),
-            ),
-          ],
-        );
-      },
+    return CupertinoPageScaffold(
+      child: Consumer<AppStateModel>(
+        builder: (context, model, child) {
+          final products = model.getProducts();
+          return CustomScrollView(
+            semanticChildCount: products.length,
+            slivers: <Widget>[
+              CupertinoSliverNavigationBar(
+                largeTitle: const Text('Cupertino Store'),
+              ),
+              SliverSafeArea(
+                // BEGINNING OF NEW CONTENT
+                top: false,
+                minimum: const EdgeInsets.only(top: 8),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      if (index < products.length) {
+                        return ProductRowItem(
+                          index: index,
+                          product: products[index],
+                          lastItem: index == products.length - 1,
+                        );
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+              ) // END OF NEW CONTENT
+            ],
+          );
+        },
+      ),
     );
   }
 }
